92e950e0b08d72257815cfdf18b4b5d84fe48942
suhaikang <qwer1234>
2023-11-29 22:40:11 +0800

    上传代码

---
+++ 源码/jsp_tour_sale/src/servlet/AdminUserDeleteServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "admin_user_delete",urlPatterns = "/admin/user_delete")
+public class AdminUserDeleteServlet extends HttpServlet {
+    private UserService uService = new UserService();
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        boolean isSuccess = uService.delete(id);
+        if(isSuccess) {
+            request.setAttribute("msg", "游客删除成功");
+        }else {
+            request.setAttribute("failMsg", "游客有下的订单，请先删除该游客下的订单，再来删除游客！");
+        }
+        request.getRequestDispatcher("/admin/user_list").forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+}
